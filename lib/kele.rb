223c102f67b88aec6ead669c6ed8a8841f425c75
Samantha <roguegoatstudios@outlook.com>
2017-05-31 22:38:40 +0000

    token error/not working properly

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,21 @@
 require 'httparty'
 
 class Kele
+attr_reader :email, :password
+  include HTTParty
+#   base_uri "https://www.bloc.io/api/v1/"
+
+  def initialize(email, password)
+    base_uri = 'https://www.bloc.io/api/v1/sessions'
+    response = self.class.post(base_uri, body: {email: email, password: password})
+    if response.code == 401
+       puts "That user doesn't, can't, and won't exist, please try again!"
+     else
+       @auth_token = response['auth_token']
+    end
+
+  end
+
+
 
 end
