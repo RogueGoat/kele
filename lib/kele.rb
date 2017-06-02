d20925003e9f6d03c7e212a1eaff95c1c5969fcd
Samantha <roguegoatstudios@outlook.com>
2017-06-02 21:51:56 +0000

    kele retreiving auth token for valid student

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,19 +1,22 @@
 require 'httparty'
+require 'json'
 
 class Kele
 attr_reader :email, :password
   include HTTParty
-#   base_uri "https://www.bloc.io/api/v1/"
+  # api_url = "https://www.bloc.io/api/v1/"
 
   def initialize(email, password)
-    base_uri = 'https://www.bloc.io/api/v1/sessions'
-    response = self.class.post(base_uri, body: {email: email, password: password})
-    if response.code == 401
-       puts "That user doesn't, can't, and won't exist, please try again!"
-     else
-       @auth_token = response['auth_token']
-    end
-
+    api_url = 'https://www.bloc.io/api/v1/sessions'
+    response = self.class.post(api_url, body: { email: email, password: password })
+    puts "Invalid credentials! Please enter a valid email and/or password" if response.code == 404
+    @auth_token = response['auth_token']
+    puts @auth_token
+  end
+  
+  def get_me
+    response = self.class.get(url, headers: { 'authorization' => @auth_token })
+    @user = JSON.parse(response.body)
   end
 
 
