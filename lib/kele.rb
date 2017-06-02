41f73b23cc8beb1a0af45db2b26cef2917bec145
Samantha <roguegoatstudios@outlook.com>
2017-06-02 22:54:31 +0000

    returns mentor availability - ryan has no availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,23 +7,35 @@ attr_reader :email, :password
   base_url = "https://www.bloc.io/api/v1/"
 
   def initialize(email, password)
-    api_url = 'https://www.bloc.io/api/v1/sessions'
-    response = self.class.post(api_url, body: { email: email, password: password })
+    response = self.class.post(api_url("sessions"), body: { email: email, password: password })
     puts "Invalid credentials! Please enter a valid email and/or password" if response.code == 404
     @auth_token = response['auth_token']
     puts @auth_token
   end
   
   def get_me
-    api_url = 'https://www.bloc.io/api/v1/users/me'
-    response = self.class.get(api_url, headers: { 'authorization' => @auth_token })
+    response = self.class.get(api_url("users/me"), headers: { 'authorization' => @auth_token })
     @user = JSON.parse(response.body)
   end
+  
+  def get_mentor_availability(mentor_id)
+    # ryan's mentor id 456756
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { 'authorization' => @auth_token })
+    mentor_availability = []
+    response.each do |time|
+      if time["booked"] == nil
+        mentor_availability.push(time)
+      else
+       puts "This mentor has no availability"
+      end
+  end
+    puts mentor_availability
+  end
 
-# private
+private
 
-#   def api_url(direction)
-#     "https://www.bloc.io/api/v1/#{direction}"
-#   end
+  def api_url(direction)
+    "https://www.bloc.io/api/v1/#{direction}"
+  end
 
 end
